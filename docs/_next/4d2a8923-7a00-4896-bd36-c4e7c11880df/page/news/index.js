00b6614ccaeb66068fd32ef10160b881482be219
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2017-05-25 06:58:57 +1000

    Build

---
+++ docs/_next/4d2a8923-7a00-4896-bd36-c4e7c11880df/page/news/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/news', function() {
+            var comp = module.exports=webpackJsonp([6],{154:function(e,t,a){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.posts=void 0;var n=a(0),i=function(e){return e&&e.__esModule?e:{default:e}}(n),l=a(3),r=t.posts=[{id:"tickets-available",title:"Tickets available",date:"May 5"}];t.default=function(){return i.default.createElement(l.Wrapper,{title:"News",linkOffset:1},r.map(function(e,t){return i.default.createElement(l.NavLink,{prefetch:!0,href:"/news/"+e.id,key:t},t+1,". ",e.date," - ",e.title)}))}},380:function(e,t,a){e.exports=a(154)}},[380]);
+            return { page: comp.default }
+          })
+        
--- docs/_next/5c9a870a-98dc-4696-aac6-9288ac772c30/page/get-involved/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/get-involved', function() {
-            var comp = module.exports=webpackJsonp([8],{151:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a=n(0),o=function(e){return e&&e.__esModule?e:{default:e}}(a),r=n(4);t.default=function(){return o.default.createElement(r.Wrapper,{title:"Get Involved"},o.default.createElement("p",null,"One of the best aspects of CampJS is the community. Over the past 6 events, that community has only grown stronger. As CampJS is a community run event, there is always plenty to help with. If you want to help in any way (or just get in on all the pre-camp excitement), head over to the #campjs channel on the ",o.default.createElement("a",{href:"http://view-source-radboats.herokuapp.com/"},"View Source Slack"),"."))}},375:function(e,t,n){e.exports=n(151)}},[375]);
-            return { page: comp.default }
-          })
-        
