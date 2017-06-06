ff48503bcd41bde337e2626f640bf9867202a310
Ash Kyd <ash@kyd.com.au>
2017-06-06 20:51:15 +1000

    populate 'about'

--- pages/about.js
+++ pages/about.js
@@ -3,9 +3,81 @@ import {
 } from '../components'
 
 export default () => (
+  <div>
   <Wrapper title='About'>
     <p>CampJS is for anyone who is interested in web technology. Beginners & experts, all are welcome.</p>
     <p>Networking with other developers is arguably the most valuable aspect of a conference, yet it’s often a hurried and fleeting affair that happens in-between the schedule of a regular conference — but CampJS isn’t a regular conference. This weekend-long retreat allows everyone enough time to learn new things, relax and most importantly: create real friendships and connections.</p>
     <p>CampJS creates a unique blend of expert-led, structured content and self-directed, unstructured learning. Some content is scheduled, but the main area is reserved for hacking. The final night is reserved for demos for people to show off what they have built or learned at the event.</p>
   </Wrapper>
+  <Wrapper title='When is CampJS?'>
+    <p>
+      CampJS V will commence at 3pm Friday on the 22nd of May, and
+      run through to 9am Monday the 25th of May. The Camp will be held at <a
+      href="http://maps.google.com/maps?f=q&hl=en&geocode=&q=Lord+Somers+Camp+Parklands+Avenue+Somers&sll=-37.787844,145.025454&sspn=0.019637,0.046778&ie=UTF8&cid=-38391441,145152110,7610982389541332772&ll=-38.382642,145.160122&spn=0.023548,0.036478&z=14&iwloc=A&source=embed">Lord
+      Somers Camp, Victoria, Australia</a>.
+    </p>
+    <center><a href="http://www.panoramio.com/photo/40130986"><img src="https://cloud.githubusercontent.com/assets/43438/6496401/485d9d00-c30e-11e4-92fd-4559e1a7ff41.png" /></a></center>
+    <p>
+      The venue provides food, drink and comfortable accommodation for
+      the duration of the event. You may camp on the grounds if that is
+      your wish.
+    </p>
+  </Wrapper>
+  <Wrapper title='Transport to camp'>
+    <p>
+    <strong>A bus will be provided to take attendees to and from the venue.</strong>
+    It will travel from Tullamarine airport and back via Melbourne CBD. Interstate travellers should fly into Tullamarine airport around midday, busses will leave between 1 and 2pm. The return bus will get people back to the airport before midday.
+    </p>
+  </Wrapper>
+  <Wrapper title="What's provided?">
+    <ul>
+      <li>Comfortable Dorm-style sleeping quarters.</li>
+      <li>Meals and Drinks</li>
+      <li>Coffee (via aldi pod machine) and Tea</li>
+      <li>Power &amp; Limited Internet</li>
+      <li>Great People</li>
+      <li>Prizes</li>
+    </ul>
+  </Wrapper>
+  <Wrapper title='What to bring'>
+    <h2>What to Bring</h2>
+    <ul>
+      <li>Earplugs</li>
+      <li>Warm Clothes</li>
+      <li>Sleeping Bag + Pillow</li>
+      <li>Bathroom Equipment</li>
+      <li>Snacks to Share</li>
+      <li>Computer, Phone &amp; Associated Chargers</li>
+      <li>Board/Card Games</li>
+      <li>Headphones</li>
+      <li>Instruments</li>
+      <li>Medications</li>
+      <li>Warm Clothes &amp; Earplugs</li>
+      <li>Good Attitude</li>
+      <li>Anything else you need to survive the weekend</li>
+    </ul>
+
+  </Wrapper>
+  <Wrapper title='Should you attend?'>
+    <p>
+    Yes you should.
+    </p>
+    <center>
+      <a href="https://twitter.com/hopefulcyborg/status/429331153611280384"><img src="https://cloud.githubusercontent.com/assets/43438/6493832/656db148-c2f8-11e4-8aeb-6f5c38c3df9a.png" /></a>
+    </center>
+
+    <p>To get a vibe for the event please check out the <a href="#videos">Videos</a>, <a href="https://plus.google.com/communities/106906604683614333625/photos/all">Photos</a> &amp; <a href="https://storify.com/campjs">Tweets</a> from previous events.</p>
+    <iframe width="100%" height="400" src="https://www.youtube.com/embed/4n3UMWHQE-o" frameborder="0" allowfullscreen></iframe>
+  </Wrapper>
+  <Wrapper title="Code of Conduct">
+      <p>CampJS takes pride in being accessible and welcoming for people
+      from all walks of life. Participants should to review our <a
+      href="code-of-conduct.html">code of conduct</a> prior to attending
+    as any violations will be taken seriously.
+    </p>
+    <p>
+      <strong><a href="code-of-conduct.html">View the CampJS Code of Conduct</a></strong>
+    </p>
+  </Wrapper>
+  </div>
 )
