76eb5c8940a6803ba946287250bddcd98c582f2e
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2017-07-08 01:12:11 +1000

    Build

---
+++ docs/_next/61ac7d7c-93fe-4373-abd8-1e4991e933bd/page/call-for-speakers/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/call-for-speakers', function() {
+            var comp = module.exports=webpackJsonp([13],{151:function(e,t,l){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a=l(0),r=function(e){return e&&e.__esModule?e:{default:e}}(a),u=l(1);t.default=function(){return r.default.createElement(u.Wrapper,{title:"Call For Speakers"},r.default.createElement("p",null,"We encourage talks for all skill levels with a broad set of topics. If you find it interesting, we want to hear about it."),r.default.createElement("p",null,"Feel free to submit multiple talks."),r.default.createElement("p",null,"Call for papers close Monday 10th July."),r.default.createElement("p",null,r.default.createElement("a",{href:"https://goo.gl/forms/cxUh8JrQccUBzw992"},"Submit your talk"),"."))}},383:function(e,t,l){e.exports=l(151)}},[383]);
+            return { page: comp.default }
+          })
+        
