516ca04f5db9260ce3114d0f1b782dbec7f12531
Sharkie <craig.sharkie@gmail.com>
2017-07-13 00:07:17 +1000

    Publishing the Build results

---
+++ docs/_next/f31e3f9c-5322-4428-aac5-23b05e7d1b22/page/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([8],{154:function(e,t,r){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a=r(0),n=function(e){return e&&e.__esModule?e:{default:e}}(a),l=r(1);t.default=function(){return n.default.createElement(l.Wrapper,null,n.default.createElement(l.Box,{is:"nav",display:"flex",flexDirection:"column"},n.default.createElement(l.NavLink,{prefetch:!0,href:"/news"},"1. News"),n.default.createElement(l.NavLink,{prefetch:!0,href:"/about"},"2. About"),n.default.createElement(l.NavLink,{prefetch:!0,href:"/schedule"},"3. Schedule"),n.default.createElement(l.NavLink,{prefetch:!0,href:"/get-involved"},"4. Get involved"),n.default.createElement(l.NavLink,{prefetch:!0,href:"/call-for-speakers"},"5. Call for speakers"),n.default.createElement(l.NavLink,{prefetch:!0,href:"/code-of-conduct"},"6. Code of conduct"),n.default.createElement(l.NavLink,{prefetch:!0,href:"/sponsors"},"7. Sponsors"),n.default.createElement(l.NavLink,{href:"https://ti.to/campjs/campjs-viii"},"8. Tickets!")))}},384:function(e,t,r){e.exports=r(154)}},[384]);
+            return { page: comp.default }
+          })
+        
