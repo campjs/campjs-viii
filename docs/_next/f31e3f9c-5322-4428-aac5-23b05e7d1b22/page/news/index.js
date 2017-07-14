84d506757b8b7269da17504e777a4348c9f9f318
Sharkie <craig.sharkie@gmail.com>
2017-07-14 22:46:12 +1000

    Build

--- docs/_next/f31e3f9c-5322-4428-aac5-23b05e7d1b22/page/news/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/news', function() {
-            var comp = module.exports=webpackJsonp([7],{155:function(e,t,a){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.posts=void 0;var n=a(0),i=function(e){return e&&e.__esModule?e:{default:e}}(n),l=a(1),r=t.posts=[{id:"tickets-available",title:"Tickets available",date:"May 5"}];t.default=function(){return i.default.createElement(l.Wrapper,{title:"News",linkOffset:1},r.map(function(e,t){return i.default.createElement(l.NavLink,{prefetch:!0,href:"/news/"+e.id,key:t},t+1,". ",e.date," - ",e.title)}))}},385:function(e,t,a){e.exports=a(155)}},[385]);
-            return { page: comp.default }
-          })
-        
