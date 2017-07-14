40fcc8a6eac125de785a3e529235c9aedd42987e
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2017-07-14 10:50:56 +1000

    Build

--- docs/_next/7d2ebd9a-4d1c-4077-ba2f-c8aa4611f2d2/page/sponsors/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/sponsors', function() {
-            var comp = module.exports=webpackJsonp([5],{165:function(e,t,o){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a=o(0),n=function(e){return e&&e.__esModule?e:{default:e}}(a),r=o(1);t.default=function(){return n.default.createElement(r.Wrapper,{title:"Become a Sponsor"},n.default.createElement("p",null,"The only thing better than being involved in CampJS as a Camper is being involved as a Sponsor*."),n.default.createElement("p",null,"We have several Sponsor prospectuses available and would love for you to get in touch with us to craft a package to meet your needs. ",n.default.createElement("a",{href:"mailto:sponsors@campjs.com/"},"Request a prospectus")," and see where you can help out. Any help is appreciated."),n.default.createElement(r.Text,{is:"h1",fontSize:1,mt:5,bold:!0},"Our Current Sponsors"),n.default.createElement(r.Sponsor,{company:"Lookahead Search",logo:"lookahead",siteUrl:"https://www.lookahead.com.au/"}))}},404:function(e,t,o){e.exports=o(165)}},[404]);
-            return { page: comp.default }
-          })
-        
