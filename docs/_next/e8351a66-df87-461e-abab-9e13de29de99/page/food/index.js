bbed2586d74e19b51757983a981ef169e8bac175
Anna Gerber <anna.m.gerber@gmail.com>
2017-07-15 13:24:33 +1000

    Added three more sessions from final batch of submissions

---
+++ docs/_next/e8351a66-df87-461e-abab-9e13de29de99/page/food/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/food', function() {
+            var comp = module.exports=webpackJsonp([14],{529:function(e,t,a){e.exports=a(530)},530:function(e,t,a){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r=a(0),o=function(e){return e&&e.__esModule?e:{default:e}}(r),n=a(3);t.default=function(){return o.default.createElement("div",null,o.default.createElement(n.Wrapper,{title:"Food and Drinks"},o.default.createElement(n.Text,null,"All meals are catered by Lord Somers Camp, snacks, tea and coffee, juice, water and flavoured milk are also included in your ticket price. There will be a coffee pod machine for attendees to use."),o.default.createElement(n.Text,null,"Beer and wine will be provided and small amounts of BYO alcohol are permitted. Please drink responsibly, if you are intoxicated and your behaviour is causing concerns you will be given plenty of water and asked to go to bed. You may be asked to leave if your behaviour is impacting other campers or if you don't comply with organiser's requests."),o.default.createElement(n.Text,null,"If you have any specific dietary requirements please note the details on your registration form. We are able to cater to most requirements given prior notice.")))}}},[529]);
+            return { page: comp.default }
+          })
+        
