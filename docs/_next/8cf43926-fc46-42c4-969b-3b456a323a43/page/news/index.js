68a3f65743c3a69be00cd4a864396a689c7b4732
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2017-07-16 22:32:56 +1000

    Build

---
+++ docs/_next/8cf43926-fc46-42c4-969b-3b456a323a43/page/news/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/news', function() {
+            var comp = module.exports=webpackJsonp([9],{161:function(e,t,i){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.posts=void 0;var a=i(0),l=function(e){return e&&e.__esModule?e:{default:e}}(a),n=i(1),s=t.posts=[{id:"tickets-available",title:"Tickets available",date:"May 5"},{id:"diversity-and-inclusion",title:"Diversity and inclusion",date:"July 5"},{id:"cfp-closed",title:"Call for Papers has now closed",date:"July 11"}];t.default=function(){return l.default.createElement(n.Wrapper,{title:"News",linkOffset:1},s.map(function(e,t){return l.default.createElement(n.NavLink,{prefetch:!0,href:"/news/"+e.id,key:t},t+1,". ",e.date," - ",e.title)}))}},400:function(e,t,i){e.exports=i(161)}},[400]);
+            return { page: comp.default }
+          })
+        
--- docs/_next/e8351a66-df87-461e-abab-9e13de29de99/page/news/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/news', function() {
-            var comp = module.exports=webpackJsonp([9],{539:function(e,t,i){e.exports=i(540)},540:function(e,t,i){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.posts=void 0;var a=i(0),l=function(e){return e&&e.__esModule?e:{default:e}}(a),n=i(3),s=t.posts=[{id:"tickets-available",title:"Tickets available",date:"May 5"},{id:"diversity-and-inclusion",title:"Diversity and inclusion",date:"July 5"},{id:"cfp-closed",title:"Call for Papers has now closed",date:"July 11"}];t.default=function(){return l.default.createElement(n.Wrapper,{title:"News",linkOffset:1},s.map(function(e,t){return l.default.createElement(n.NavLink,{prefetch:!0,href:"/news/"+e.id,key:t},t+1,". ",e.date," - ",e.title)}))}}},[539]);
-            return { page: comp.default }
-          })
-        
