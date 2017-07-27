708746872e2db8821e90abaf998b538c269d71b2
Sharkie <craig.sharkie@gmail.com>
2017-07-28 00:46:38 +1000

    Building

---
+++ docs/_next/cb2c1ede-32e2-4b1b-a449-f696b0b5ccfb/page/call-for-speakers/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/call-for-speakers', function() {
+            var comp = module.exports=webpackJsonp([20],{153:function(e,t,l){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a=l(0),n=function(e){return e&&e.__esModule?e:{default:e}}(a),o=l(1);t.default=function(){return n.default.createElement(o.Wrapper,{title:"Call For Speakers"},n.default.createElement("p",null,"We encourage talks for all skill levels with a broad set of topics. If you find it interesting, we want to hear about it."),n.default.createElement(o.Text,{bold:!0,fontSize:"25px"},"Call for papers has now closed."),n.default.createElement("p",null,"Everyone who has submitted will recieve confirmation ASAP and no later than Friday 14th July."))}},395:function(e,t,l){e.exports=l(153)}},[395]);
+            return { page: comp.default }
+          })
+        
