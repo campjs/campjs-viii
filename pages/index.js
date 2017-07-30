efcf1401e533dc09f88c4786544869ea633e1ded
Anders Rasmussen <divinegod@gmail.com>
2017-07-30 22:36:15 +1000

    Implement schedule. Move Sessions to new page

--- pages/index.js
+++ pages/index.js
@@ -12,18 +12,19 @@ export default () => (
     >
       <NavLink prefetch href='/news'>1. News</NavLink>
       <NavLink prefetch href='/about'>2. About</NavLink>
-      <NavLink prefetch href='/schedule'>3. Sessions</NavLink>
-      <NavLink prefetch href='/get-involved'>4. Get involved</NavLink>
-      <NavLink prefetch href='/call-for-speakers'>5. Call for speakers</NavLink>
-      <NavLink prefetch href='/code-of-conduct'>6. Code of conduct</NavLink>
-      <NavLink prefetch href='/organisers'>7. Organisers</NavLink>
-      <NavLink prefetch href='/feedback'>8. Feedback</NavLink>
-      <NavLink href='https://ti.to/campjs/campjs-viii'>9. Tickets!</NavLink>
-      <NavLink prefetch href='/accommodation'>10. Accommodation</NavLink>
-      <NavLink prefetch href='/food'>11. Food and Drinks</NavLink>
-      <NavLink prefetch href='/transport'>12. Transport</NavLink>
-      <NavLink prefetch href='/accessibility'>13. Accessibility</NavLink>
-      <NavLink prefetch href='/sponsors'>14. Sponsors</NavLink>
+      <NavLink prefetch href='/schedule'>3. Schedule</NavLink>
+      <NavLink prefetch href='/sessions'>4. Sessions</NavLink>
+      <NavLink prefetch href='/get-involved'>5. Get involved</NavLink>
+      <NavLink prefetch href='/call-for-speakers'>6. Call for speakers</NavLink>
+      <NavLink prefetch href='/code-of-conduct'>7. Code of conduct</NavLink>
+      <NavLink prefetch href='/organisers'>8. Organisers</NavLink>
+      <NavLink prefetch href='/feedback'>9. Feedback</NavLink>
+      <NavLink href='https://ti.to/campjs/campjs-viii'>10. Tickets!</NavLink>
+      <NavLink prefetch href='/accommodation'>11. Accommodation</NavLink>
+      <NavLink prefetch href='/food'>12. Food and Drinks</NavLink>
+      <NavLink prefetch href='/transport'>13. Transport</NavLink>
+      <NavLink prefetch href='/accessibility'>14. Accessibility</NavLink>
+      <NavLink prefetch href='/sponsors'>15. Sponsors</NavLink>
     </Box>
   </Wrapper>
 )
