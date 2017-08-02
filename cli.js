6bc9fdbefd18884b14e7cb579ff2222510e6aa66
Adam Hoddinott <adam.hoddinott@salespreso.com>
2017-08-03 08:44:06 +1000

    Allow port to be changed with cli args

--- cli.js
+++ cli.js
@@ -1,11 +1,26 @@
 #!/usr/bin/env node
 
+const ArgumentParser = require('argparse').ArgumentParser;
+const parser = new ArgumentParser({
+  addHelp: true,
+  description: 'Offline version of the CampJS website'
+});
+
+parser.addArgument(
+  ['-p', '--port'],
+  {
+    help: 'Port to run localserver on. Defaults to 3000.'
+  }
+);
+
+const args = parser.parseArgs();
+
 const express = require('express')
 const app = express()
 
 app.use(express.static('docs'))
 
-const port = parseInt(process.env.PORT || 3000)
-app.listen(3000, function () {
+const port = parseInt(args.port || 3000)
+app.listen(port, function () {
   console.log(`Welcome to CampJS. Open up http://localhost:${port}`);
 })
